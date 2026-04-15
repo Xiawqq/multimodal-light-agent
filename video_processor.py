30e6cd773c9fc666e706105add67067182e2d040
Xiawqq <2378037779@qq.com>
2026-04-15 23:16:02 +0800

    init: basic project structure and modules

--- video_processor.py
+++ video_processor.py
@@ -1,5 +1,7 @@
 import cv2
 
+
+# 播放完整视频
 def play_video(video_path):
 
     cap = cv2.VideoCapture(video_path)
@@ -24,6 +26,8 @@ def play_video(video_path):
     cap.release()
     cv2.destroyAllWindows()
 
+
+# 展示视频第一帧
 def show_first_frame(video_path):
     cap=cv2.VideoCapture(video_path)
 
@@ -42,6 +46,34 @@ def show_first_frame(video_path):
     cap.release()
     cv2.destroyAllWindows()
 
+
+# 计算视频总帧数
+def count_frames(video_path: str):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return -1
+
+    frame_count = 0
+
+    while 1:
+        ret, frame = cap.read()
+        if ret == False :
+            break
+        else:
+            frame_count += 1
+
+    cap.release()
+    return frame_count
+
+
+# 计算视频总帧数 + 展示第一帧 + 交互说明
 def process_video(video_path: str):
-    show_first_frame(video_path)
-    return f"已成功读取并展示视频第一帧：{video_path}"
+    frame_count = count_frames(video_path)
+
+    if frame_count == -1:
+        return "视频打开失败"
+    else:
+        show_first_frame(video_path)
+
+    return f"已成功读取并展示视频第一帧，视频总帧数为：{frame_count}"

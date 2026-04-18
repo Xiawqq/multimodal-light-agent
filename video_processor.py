c128fe019f24a09b52b142c4ecbd20ba4344a3bb
Xiawqq <2378037779@qq.com>
2026-04-18 20:22:57 +0800

    fix：修复一个小bug，保证每次视频打开后都被释放

--- video_processor.py
+++ video_processor.py
@@ -67,6 +67,7 @@ def get_video_frame_count(video_path: str):
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     if frame_count <= 0:    # 视频打开了有没有内容
+        cap.release()
         return "视频打开失败或无法获取视频信息"
 
     cap.release()
@@ -78,15 +79,16 @@ def get_video_fps(video_path: str):
     cap = open_video(video_path)
 
     if cap is None:     # 视频能不能打开
-        return -1
+        return "视频打开失败或无法获取视频帧率信息"
 
     fps = cap.get(cv2.CAP_PROP_FPS)
 
     if fps <= 0:    # 视频打开了有没有内容
-        return -1
+        cap.release()
+        return "视频打开失败或无法获取视频帧率信息"
 
     cap.release()
-    return f"视频总帧数为：{fps}"
+    return f"视频帧率约为：{fps:.2f} FPS"
 
 
 # 计算视频时长
@@ -101,6 +103,7 @@ def get_video_duration(video_path: str):
     fps = cap.get(cv2.CAP_PROP_FPS)
 
     if frame_count <= 0 or fps <= 0:
+        cap.release()
         return "视频打开失败或无法获取视频信息"
 
     duration = frame_count / fps

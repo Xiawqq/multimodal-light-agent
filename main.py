6cbfbdff26d1dbb35b94b3ddcf30a1296550c414
Xiawqq <2378037779@qq.com>
2026-04-18 19:03:34 +0800

    feat：交互逻辑优化 主要是对文件打开问题后出现问题的情况进行了统一管理和反馈

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
 import os
+import cv2
 import router
 import video_processor
 import analysis
 import answer_generator
 
 
+# 检测视频文件是否能正常读取
+def is_video_readable(video_path: str) -> bool:     # 规定返回结果为布尔值
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        return False
+    cap.release()
+    return True
+
+
 def main():
 
-    question = input("请输入你的问题：")
+    # 确保有真实问题输入
+    question = input("请输入你的问题：").strip()
+
+    if not question:
+        print("系统回答：问题不能为空，请重新输入。")
+        return
+
+    # 当前版本仅支持开放的一些模态
     task_type = router.route_question(question)
 
+    if task_type not in ["video", "image", "text"]:
+        print("系统回答：暂时无法识别你的问题类型。")
+        return
+
+    # 确实问题可以被当前的多模态系统处理，开始处理
     print("系统识别的大类任务是：", task_type)
 
+    # # 视频模态
     if task_type == "video":
 
         video_path = input("请输入视频文件路径：").strip()
+
+        # # # 打开视频出现问题的情况
+        if not video_path:
+            print("系统回答：视频路径不能为空。")
+            return
 
         if not os.path.exists(video_path):
             print("系统回答：视频文件不存在，请检查路径是否正确。")
             return
 
+        if not is_video_readable(video_path):
+            print("系统回答：视频文件无法打开，请确认文件格式是否正确。")
+            return
+
+        # # # 分析视频模态子任务，并进入对应的模块
         task_detail = router.route_video_question(question)
         print("系统识别的视频子任务是：", task_detail)
 
@@ -42,10 +75,10 @@ def main():
         print(answer)
 
     elif task_type == "image":
-        print("系统回答：暂时还不支持图像处理。")
+        print("系统回答：当前版本暂时还不支持图像处理。")
 
     else:
-        print("系统回答：这是普通文本问题，当前版本暂时不处理。")
+        print("系统回答：当前版本暂时还不支持普通文本问题处理。")
 
 
 if __name__ == "__main__":

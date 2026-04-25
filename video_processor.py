603d7be50da6214904a14ce45d85403a4722dac6
Xiawqq <2378037779@qq.com>
2026-04-26 00:53:18 +0800

    chore：删除了早期测试时期的函数

--- video_processor.py
+++ video_processor.py
@@ -9,53 +9,6 @@ import cv2
 from utils import open_video
 
 
-# 播放完整视频
-def play_video(video_path):
-
-    cap = open_video(video_path)
-
-    if cap is None:
-        print("视频打开失败")
-        return
-
-    while 1:
-        ret,frame=cap.read()
-
-        if ret==False:
-            print("视频播放结束")
-            break
-
-        cv2.imshow("frame",frame)
-
-        if cv2.waitKey(30)==27:
-            print("用户手动退出播放")
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-# 展示视频第一帧
-def show_first_frame(video_path):
-
-    cap = open_video(video_path)
-
-    if cap is None:
-        print("视频打开失败")
-        return
-
-    ret, frame = cap.read()
-
-    if ret:
-        cv2.imshow("first frame", frame)
-        cv2.waitKey(0)
-    else:
-        print("无法读取第一帧")
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
 # 计算视频总帧数
 def get_video_frame_count(video_path: str):
 
